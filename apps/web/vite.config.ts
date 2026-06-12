2bedf035e177b5735200e3ffbbc78394bdd22972
Chandrasekhar <guntreddichandrasekhar2005@gmail.com>
2026-06-12 19:54:42 +0530

    fix project config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,8 @@
 import path from 'node:path';
 import { reactRouter } from '@react-router/dev/vite';
-import { reactRouterHonoServer } from 'react-router-hono-server/dev';
+//import { reactRouterHonoServer } from 'react-router-hono-server/dev';
 import { defineConfig } from 'vite';
-import babel from 'vite-plugin-babel';
+//import babel from 'vite-plugin-babel';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { addRenderIds } from './plugins/addRenderIds';
 import { aliases } from './plugins/aliases';
@@ -12,10 +12,12 @@ import { loadFontsFromTailwindSource } from './plugins/loadFontsFromTailwindSour
 import { nextPublicProcessEnv } from './plugins/nextPublicProcessEnv';
 import { restart } from './plugins/restart';
 import { restartEnvFileChange } from './plugins/restartEnvFileChange';
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   // Keep them available via import.meta.env.NEXT_PUBLIC_*
-  envPrefix: 'NEXT_PUBLIC_',
+  
+    envPrefix: 'NEXT_PUBLIC_',
   build: {
     target: 'esnext',
   },
@@ -38,40 +40,33 @@ export default defineConfig({
     ],
   },
   logLevel: 'info',
-  plugins: [
-    nextPublicProcessEnv(),
-    restartEnvFileChange(),
-    reactRouterHonoServer({
-      serverEntryPoint: './__create/index.ts',
-      runtime: 'node',
-    }),
-    babel({
-      include: ['src/**/*.{js,jsx,ts,tsx}'], // or RegExp: /src\/.*\.[tj]sx?$/
-      exclude: /node_modules/, // skip everything else
-      babelConfig: {
-        babelrc: false, // don’t merge other Babel files
-        configFile: false,
-        plugins: ['styled-jsx/babel'],
-      },
-    }),
-    restart({
-      restart: [
-        'src/**/page.jsx',
-        'src/**/page.tsx',
-        'src/**/layout.jsx',
-        'src/**/layout.tsx',
-        'src/**/route.js',
-        'src/**/route.ts',
-      ],
-    }),
-    consoleToParent(),
-    loadFontsFromTailwindSource(),
-    addRenderIds(),
-    reactRouter(),
-    tsconfigPaths(),
-    aliases(),
-    layoutWrapperPlugin(),
-  ],
+ plugins: [
+  react(),
+  nextPublicProcessEnv(),
+  restartEnvFileChange(),
+
+  // Optional dev tools
+  restart({
+    restart: [
+      'src/**/page.jsx',
+      'src/**/page.tsx',
+      'src/**/layout.jsx',
+      'src/**/layout.tsx',
+      'src/**/route.js',
+      'src/**/route.ts',
+    ],
+  }),
+
+  consoleToParent(),
+  loadFontsFromTailwindSource(),
+  addRenderIds(),
+
+  reactRouter(),
+  tsconfigPaths(),
+  aliases(),
+  layoutWrapperPlugin(),
+],
+    
   resolve: {
     alias: {
       lodash: 'lodash-es',
